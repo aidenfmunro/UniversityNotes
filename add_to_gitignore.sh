a4da66ccc40f8b543d99b3641bca041345e468c2
aidenfmunro <aidenfmunro@gmail.com>
2025-01-31 21:15:45 +0300

    4 sem add

--- add_to_gitignore.sh
+++ add_to_gitignore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Path to the university folder
-UNIVERSITY_DIR=~/UniversityNotes
+# GitHub file size limit in bytes (50MB = 54857600 bytes)
+LIMIT=50857600
 
 # Find files over 100 MB and append them to .gitignore
 find "$UNIVERSITY_DIR" -type f -size +100M -print >> .gitignore
